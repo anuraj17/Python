03ff60c47369eefcdfeb3d200380231bfea3317b
anuraj17 <anurajguru@icloud.com>
2020-10-12 21:40:53 -0700

    Added MergeSort and Common Printer and Verify
    Signed-off-by: anuraj17 <anurajguru@icloud.com>

--- sorting.py
+++ sorting.py
@@ -5,53 +5,115 @@ import random
 
 #Data Generator
 randomData = []
-for i in range(0, 20):
+for i in range(0, 7):
     randomData.append(random.randint(0, 50))
-print("Random Data")
-print(randomData)
 
 #Printer Function
 def printOutput(data):
-    print("\nSorted Data")
     print(data)
 
 #Verify Sorting
-def Verify(arr):
-    for i in range(0, len(arr) - 1):
-        if(arr[i] > arr[i + 1]):
+def Verify(data):
+    
+    print("Result Data:")
+    printOutput(data)
+
+    for i in range(0, len(data) - 1):
+        if(data[i] > data[i + 1]):
             print("Sorting Failed")
             exit(0)
     print("Sorting Successfull")
 
+algorithmSelector = 2
+
+#********************************1: QUICKSORT BEGINS***************************************************
 """
 QuickSort:
-Choose the last element as the Partition recursively
+Choose the last element as the Pivot recursively
+Advantages:
+Disadvantages:
+Time Complexity:
+Space Complexity:
 """
-def quickSort(arr, low, high):
+def quickSort(data, low, high):
     if(low < high):
-        pi = quickSortPartition(arr, low, high)
-        quickSort(arr, low, pi - 1)
-        quickSort(arr, pi + 1, high)
+        pi = quickSortPartition(data, low, high)
+        quickSort(data, low, pi - 1)
+        quickSort(data, pi + 1, high)
 
-def swap(arr, i, j):
-    temp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = temp
-    return arr
+def swap(data, i, j):
+    temp = data[i]
+    data[i] = data[j]
+    data[j] = temp
+    return data
 
-def quickSortPartition(arr, low, high):
-    pivot = arr[high]
+def quickSortPartition(data, low, high):
+    pivot = data[high]
 
     i = low - 1
 
     for k in range(low, high):
-        if(arr[k] < pivot):
+        if(data[k] < pivot):
             i = i + 1
-            swap(arr, i, k)
+            swap(data, i, k)
     
-    swap(arr, i + 1, high)
+    swap(data, i + 1, high)
     return i + 1
 
-quickSort(randomData, 0, len(randomData) - 1)
-printOutput(randomData)
-Verify(randomData)
+if (algorithmSelector == 1):
+    print("Random Data")
+    printOutput(randomData)
+    quickSortData = list(randomData)
+    quickSort(quickSortData, 0, len(quickSortData) - 1)
+    Verify(quickSortData)
+#*********************************QUICKSORT ENDS*******************************************************
+
+#********************************2: MERGESORT BEGINS***************************************************
+"""
+MergeSort:
+Split till there is only one element, and then sort while combining them back
+Advantages:
+Disadvantages:
+Time Complexity:
+Space Complexity:
+"""
+
+def mergeSort(data):
+    if(len(data) > 1):
+
+        mid = len(data)//2
+        left = data[:mid]
+        right = data[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i = j = k = 0
+
+        while(i < len(left) and j < len(right)):
+            if(left[i] < right[j]):
+                data[k] = left[i]
+                i = i + 1
+            
+            else:
+                data[k] = right[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(left):
+            data[k] = left[i]
+            k = k + 1
+            i = i + 1
+
+        while j < len(right):
+            data[k] = right[j]
+            k = k + 1
+            j = j + 1   
+        
+if (algorithmSelector == 2):
+    print("Random Data")
+    printOutput(randomData)
+    mergeSortData = list(randomData)
+    mergeSort(mergeSortData)
+    Verify(mergeSortData)
+#********************************MERGESORT ENDS***************************************************
